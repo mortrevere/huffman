91e4ff1593ed04a73af741bdcb30c4d140471d6b
Kleme <Kleme@192.168.1.19>
2016-04-02 22:15:38 +0200

    Added recursive organize() method that do the main job

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class tree:
     def __init__(self, children = []):
         self.parent = None
-        self.children = children.copy()
+        self.setChildren(children)
         self.w = 0
         self.setW()
     
@@ -21,33 +21,41 @@ class tree:
         for child in children:
             self.addChild(child)
 
+    def setChildren(self, children):
+        self.children = []
+        self.addChildren(children)
+
     def setW(self):
         self.w = 0
         for child in self.children:
             self.w += child.w 
+            
     def sort(self):
         self.children = sorted(self.children, key=lambda a: a.w)
+        
+    def organize(self):
+        while len(self.children) > 2 :
+            self.sort()
+            self.setChildren([tree([self.children[0],self.children[1]])] + self.children[2:]) #Il faut jamais modifier le self.children directement
+        for child in self.children:
+            child.organize()
 
 class leaf:
     def __init__(self, value, w):
         self.parent = None
         self.value = value
         self.w = w
+        
     def print(self, lvl = 0):
         print("--" * lvl + "({}, {})".format(self.value, self.w))
-    
-    
-
-'''t = tree()
-for i in range(10):
-    t.addChild(leaf(i,i))
-
-eval("t")'''
+        
+    def organize(self):
+        return True
 
 t = tree()
 dick = {}
 
-with open("mathinfo.py") as f:
+with open("main.py") as f:
     for line in f:
         for c in line.strip():
             dick[c] = dick.get(c,0) + 1
@@ -56,5 +64,5 @@ for c in dick.keys():
     t.addChild(leaf(c,dick[c]))
 
 t.print()
-t.sort()
+t.organize()
 t.print()
