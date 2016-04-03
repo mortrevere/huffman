a459facd5d26a86106d099909096a9d39ea2a434
mortrevere <leo.vincent1337@gmail.com>
2016-04-03 15:13:33 +0200

    improved self tests

--- src/huffman/main.py
+++ src/huffman/main.py
@@ -3,12 +3,12 @@
 from codec import *
 
 io = codec()
-io.load("bf.txt")
+io.load("a_very_short_story.txt")
 
 source = io.buf
 io.encode()
 
-print("Filesize is down by", (io.stats['sourceLen']/io.stats['outLen'])*100, "%")
+print("Space saved : ", 100 - (io.stats['outLen']/io.stats['sourceLen'])*100, "%")
 
 io.decode()
 
@@ -18,4 +18,17 @@ print('Self-test result : ', end = '')
 if source == out:
     print('ok')
 else:
-    print('fail')
+    print('fail. \n\nDetails : ')
+    if len(source) != len(out):
+        delta = len(source) - len(out)
+        common = min(len(source), len(out))
+        print('\t /!\ Different length :\n\t\t Source :', len(source), '\n\t\t Output :', len(out),
+              '\n\t\t Difference :', delta)
+
+        print('\t Difference on common length :')
+        for offset in range(0, delta):
+            diff = 0
+            for i in range(common):
+                if source[i] != out[i]:
+                    diff += 1
+            print('\t\t offset {} : {} %\n'.format(offset, (diff/common)*100 ))
