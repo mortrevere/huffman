da145f48b2c61d8c937a573d755449d8f6b78bd1
Kleme <Kleme@192.168.1.19>
2016-04-03 18:08:55 +0200

    Results of visualizations as images

--- src/huffman/tktree.py
+++ src/huffman/tktree.py
@@ -3,36 +3,38 @@ from tree import *
 from leaf import *
 
 
-def show(t, w, h):
+def show(t, width=30, font=8):
     win = tk.Tk()
-    cv = canv(win, t, w, h)
+    cv = canv(win, t, width, font)
     cv.pack()
     win.mainloop()
 
+def saveasimage(path, t, width=40, font=8):
+    cv = canv(None, t, width, font)
+    cv.postscript(file=path, colormode='color')
 
-def canv(parent, t, w, h):
+def canv(parent, t, w2, h2):
+    l = len(t)
+    s = t.getSize()
+    h2 *= 1.1
+    w = w2 * (2 * l + 1)
+    h = h2 * (2 * s + 1)
     cv = tk.Canvas(parent, width=w, height=h, bg="white")
-    wt = len(t)
-    ht = t.getSize()
-    print(wt)
-    w2 = w // (2 * wt + 1)
-    h2 = h // (2 * ht + 1)
-    # Generate the visual tree recursively
-    genTree(cv, w2, h // 2, h, w2, h2, t)
+    genTreeH(cv, w2, h // 2, h, w2, h2, t)
     return cv
 
 
-def genTree(cv, x0, y0, dy, w2, h2, t):
+def genTreeH(cv, x0, y0, dy, w2, h2, t):
     cv.create_rectangle(
         x0, y0 - h2 // 2, x0 + w2, y0 + h2 // 2, outline="lightgrey")
-    cv.create_text(x0 + w2 / 2, y0, font=("Lucida console", 8), text=str(t))
+    cv.create_text(
+        x0 + w2 / 2, y0, font=("Lucida console", int(h2 / 1.1)), text=str(t))
     st = t.getSize()
     dy0 = y0 - dy // 2
     if not t.isLeaf:
         for child in t.children:
             hc = int(dy * child.getSize() / st)
-            genTree(cv, x0 + w2 * 2, dy0 + hc // 2, hc, w2, h2, child)
+            genTreeH(cv, x0 + w2 * 2, dy0 + hc // 2, hc, w2, h2, child)
             cv.create_line(
                 x0 + w2, y0, x0 + w2 * 2, dy0 + hc // 2, fill="lightgrey")
             dy0 += hc
-    return True  # TODO
