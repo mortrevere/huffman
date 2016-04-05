85adc15ef211de55b88559020f258299862d9ff5
Kleme <Kleme@192.168.1.19>
2016-04-05 12:46:18 +0200

    Script runs with python -m huffman args

---
+++ src/huffman/__main__.py
@@ -0,0 +1,60 @@
+import sys
+try:
+    from . import codec
+except SystemError:
+    import codec
+import tkinter as tk
+
+
+def window():
+    c = codec.codec()
+    win = tk.Tk()
+    win.mainloop()
+    # TODO
+
+
+def compress(src, dest):
+    print("Compressing " + src + " to " + dest + " ...")
+    c = codec.codec()
+    print("Loading source file...")
+    c.load(src)
+    print("Source file loaded in {.2f} ms".format(c.stats['loadingTime']*1000))
+    print("Encoding source file...")
+    c.encode()
+    print("Source file encoded in {.2f} ms".format(c.stats['processTime']*1000))
+    print("Writing destination file...")
+    c.write(dest, enc=True)
+    print("Destination file written in {.2f} ms".format(c.stats['processTime']*1000))
+    print("Compression successful !")
+
+
+def decompress(src, dest):
+    print("Uncompressing " + src + " to " + dest + " ...")
+    c = codec.codec()
+    print("Loading source file...")
+    c.load(src)
+    print("Source file loaded in {.2f} ms".format(c.stats['loadingTime']*1000))
+    print("Decoding source file...")
+    c.encode()
+    print("Source file decoded in {.2f} ms".format(c.stats['processTime']*1000))
+    print("Writing destination file...")
+    c.write(dest, enc=True)
+    print("Destination file written in {.2f} ms".format(c.stats['processTime']*1000))
+    print("Uncompression successful !")
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 1:
+        window()
+    elif len(args) == 3:
+        c1 = args[1].split("\.")[-1] == "clh"
+        c2 = args[1].split("\.")[-1] == "clh"
+        if not c1 and not c2:
+            print("The source or the destination file should be .clh format.")
+        elif c1:
+            decompress(args[1], args[2])
+        else:
+            compress(args[1], args[2])
+    else:
+        print("Please indicate source and destination \
+        file or leave blank for the window.")
